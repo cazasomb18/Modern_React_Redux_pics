788109eb371040c90f8a4f750cb5aa06fa7115b3
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-12-12 18:12:58 -0600

    working on form functionality

---
+++ src/components/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+	state = { term: ''};
+
+	onFormSubmit = (e) => {
+		e.preventDefault();
+
+		console.log(this.state.term);
+
+	};
+	
+	componentDidMount(){
+
+
+	};
+
+	render(){
+		return(
+			<div className="ui segment">
+				<form className="ui form" onSubmit={this.onFormSubmit} action="submit">
+					<div className="field">
+						<label>Image Search</label>
+						<input 
+							placeholder="your text here" 
+							type="text" 
+							value={this.state.term} 
+							onChange={ e => this.setState({ term: e.target.value })} 
+						/>
+					</div>
+				</form>
+			</div>
+		); 
+	};
+};
+
+export default SearchBar;
